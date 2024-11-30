95bfcf2c32b67d8b044c54e57c9b5bef5a7b6d64
Choyoru <153852354+Choyoru@users.noreply.github.com>
2024-12-01 00:49:11 +0100

    v0.0.5

--- js/plugins/mood.js
+++ js/plugins/mood.js
@@ -136,20 +136,23 @@
             else if(!$gameActors.actor(1).isStateAffected(16) && $gameVariables.value(13) < 25){
                 $gameActors.actor(1).addState(16);
             }
-            else if($gameActors.actor(1).isStateAffected(17) && ($gameVariables.value(13) >= 75 || $gameVariables.value(13) < 25)) {
+
+            if($gameActors.actor(1).isStateAffected(17) && ($gameVariables.value(13) >= 75 || $gameVariables.value(13) < 25)) {
                 $gameActors.actor(1).removeState(17);
             } 
             else if(!$gameActors.actor(1).isStateAffected(17) && ($gameVariables.value(13) < 75 && $gameVariables.value(13) >= 25)){
                 $gameActors.actor(1).addState(17);
             }
-            else if ($gameActors.actor(1).isStateAffected(18) && $gameVariables.value(13) < 75) {
+
+            if ($gameActors.actor(1).isStateAffected(18) && $gameVariables.value(13) < 75) {
                 $gameActors.actor(1).removeState(18);
             } 
             else if (!$gameActors.actor(1).isStateAffected(18) && $gameVariables.value(13) >= 75) {
                 $gameActors.actor(1).addState(18);
             }
         }
-        else{
+
+        if(!$gameActors.actor(1).isStateAffected(16) && !$gameActors.actor(1).isStateAffected(17) && !$gameActors.actor(1).isStateAffected(18)){
             if($gameVariables.value(13) >= 75){
                 $gameActors.actor(1).addState(18);
             }else if($gameVariables.value(13) >= 25){
